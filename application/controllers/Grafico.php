454609399996188c97e7913ba2d84b8aa2308748
Bioniko <85115922+Bioniko@users.noreply.github.com>
2024-06-11 22:34:29 -0600

    Update Grafico.php

--- application/controllers/Grafico.php
+++ application/controllers/Grafico.php
@@ -34,8 +34,8 @@ class Grafico extends CI_Controller {
 		try{
 			$efe = $this->db->query("SELECT SUM(CASE WHEN mov_tipo_movimiento = 0 THEN mov_monto ELSE 0 END) - SUM(CASE WHEN mov_tipo_movimiento = 1 THEN mov_monto ELSE 0 END) AS EfectivoTotal FROM movimiento WHERE log_id = ".$_COOKIE['log_id']." AND mov_tipo_pago = 0;")->row();
 			$cue = $this->db->query("SELECT SUM(CASE WHEN mov_tipo_movimiento = 0 THEN mov_monto ELSE 0 END) - SUM(CASE WHEN mov_tipo_movimiento = 1 THEN mov_monto ELSE 0 END) AS EfectivoTotal FROM movimiento WHERE log_id = ".$_COOKIE['log_id']." AND mov_tipo_pago = 1;")->row();
-			$ing = $this->db->query("SELECT SUM(mov_monto) FROM movimiento WHERE log_id = 3 AND mov_tipo_movimiento = 0 AND YEAR(mov_fecha) = YEAR(CURDATE()) AND MONTH(mov_fecha) = MONTH(CURDATE());")->row();
-			$egr = $this->db->query("SELECT SUM(mov_monto) FROM movimiento WHERE log_id = 3 AND mov_tipo_movimiento = 1 AND YEAR(mov_fecha) = YEAR(CURDATE()) AND MONTH(mov_fecha) = MONTH(CURDATE());")->row();
+			$ing = $this->db->query("SELECT SUM(mov_monto) FROM movimiento WHERE log_id = ".$_COOKIE['log_id']." AND mov_tipo_movimiento = 0 AND YEAR(mov_fecha) = YEAR(CURDATE()) AND MONTH(mov_fecha) = MONTH(CURDATE());")->row();
+			$egr = $this->db->query("SELECT SUM(mov_monto) FROM movimiento WHERE log_id = ".$_COOKIE['log_id']." AND mov_tipo_movimiento = 1 AND YEAR(mov_fecha) = YEAR(CURDATE()) AND MONTH(mov_fecha) = MONTH(CURDATE());")->row();
 			$data = (object)array('efe' => $efe, 'cue' => $cue, 'ing' => $ing, 'egr' => $egr);
 			$this->load->view('grafico.php',(array)$data);
 		}catch(Exception $e){
