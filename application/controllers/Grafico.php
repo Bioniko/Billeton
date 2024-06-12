d9ac1ab2c3f63b722715cb770fdb85c191b9230a
Bioniko <85115922+Bioniko@users.noreply.github.com>
2024-06-11 22:07:57 -0600

    graficos

---
+++ application/controllers/Grafico.php
@@ -0,0 +1,45 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Grafico extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->database();
+		$this->load->helper('url');
+
+		$this->load->library('grocery_CRUD');
+	}
+
+	public function _example_output($output = null)
+	{
+		$this->load->view('grafico.php',(array)$output);
+	}
+
+	public function offices()
+	{
+		$output = $this->grocery_crud->render();
+
+		$this->_example_output($output);
+	}
+
+	public function index()
+	{
+		$this->_example_output((object)array('output' => '' , 'js_files' => array() , 'css_files' => array()));
+	}
+
+	public function show()
+	{
+		try{
+			$efe = $this->db->query("SELECT SUM(CASE WHEN mov_tipo_movimiento = 0 THEN mov_monto ELSE 0 END) - SUM(CASE WHEN mov_tipo_movimiento = 1 THEN mov_monto ELSE 0 END) AS EfectivoTotal FROM movimiento WHERE log_id = ".$_COOKIE['log_id']." AND mov_tipo_pago = 0;")->row();
+			$cue = $this->db->query("SELECT SUM(CASE WHEN mov_tipo_movimiento = 0 THEN mov_monto ELSE 0 END) - SUM(CASE WHEN mov_tipo_movimiento = 1 THEN mov_monto ELSE 0 END) AS EfectivoTotal FROM movimiento WHERE log_id = ".$_COOKIE['log_id']." AND mov_tipo_pago = 1;")->row();
+			$ing = $this->db->query("SELECT SUM(mov_monto) FROM movimiento WHERE log_id = 3 AND mov_tipo_movimiento = 0 AND YEAR(mov_fecha) = YEAR(CURDATE()) AND MONTH(mov_fecha) = MONTH(CURDATE());")->row();
+			$egr = $this->db->query("SELECT SUM(mov_monto) FROM movimiento WHERE log_id = 3 AND mov_tipo_movimiento = 1 AND YEAR(mov_fecha) = YEAR(CURDATE()) AND MONTH(mov_fecha) = MONTH(CURDATE());")->row();
+			$data = (object)array('efe' => $efe, 'cue' => $cue, 'ing' => $ing, 'egr' => $egr);
+			$this->load->view('grafico.php',(array)$data);
+		}catch(Exception $e){
+			show_error($e->getMessage().' --- '.$e->getTraceAsString());
+		}
+	}
+}
--- application/views/menu.php
+++ application/views/menu.php
@@ -5,29 +5,23 @@
                         <div class="dropdown profile-element"> <span>
                             <img alt="image" class="img-circle" src="<?php echo base_url();?>Tema/Static_Full_Version/img/profile_small.png" style="width: 40%;"/>
                              </span>
-                            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
-                             </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
-                            <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                                <li><a href="profile.html">Profile</a></li>
-                                <li><a href="contacts.html">Contacts</a></li>
-                                <li><a href="mailbox.html">Mailbox</a></li>
-                                <li class="divider"></li>
-                                <li><a href="login.html">Logout</a></li>
-                            </ul>
+                            
                         </div>
                         <div class="logo-element">
                         <img alt="image" class="img-circle" src="<?php echo base_url();?>Tema/Static_Full_Version/img/profile_small.png" style="width: 40%;"/>
                         </div>
                     </li>
                     <li>
-                        <a href="<?php echo base_url();?>index.php/Login/Show"><i class="fa-solid fa-right-to-bracket"></i> <span class="nav-label">Login</span></a>
+                        <a href="<?php echo base_url();?>index.php/Grafico/Show"><i class="fa-solid fa-chart-pie"></i> <span class="nav-label">Grafico</span></a>
                     </li>
                     <li>
                         <a href="<?php echo base_url();?>index.php/Comercios/Show"><i class="fa-solid fa-cash-register"></i> <span class="nav-label">Comercios</span></a>
                     </li>
                     <li>
                         <a href="<?php echo base_url();?>index.php/Movimiento/Show"><i class="fa-solid fa-sack-dollar"></i> <span class="nav-label">Movimiento</span></a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url();?>index.php/Login/Show"><i class="fa-solid fa-right-to-bracket"></i> <span class="nav-label">Login</span></a>
                     </li>
                 </ul>
 
