98b107d029894877a47f5e7392a8e2f5ce047939
Bioniko <85115922+Bioniko@users.noreply.github.com>
2024-06-27 23:31:15 -0600

    Update head.php

--- application/views/head.php
+++ application/views/head.php
@@ -8,7 +8,8 @@ setlocale(LC_TIME, 'es_ES.UTF-8');
 ?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>INSPINIA | Data Tables</title>
+<title>Billeton</title>
+<link rel="icon" href="<?php echo base_url();?>Tema/Static_Full_Version/img/profile_small.png" type="image/png">
 <link href="<?php echo base_url();?>Tema/Static_Full_Version/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?php echo base_url();?>Tema/Static_Full_Version/font-awesome/css/font-awesome.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
